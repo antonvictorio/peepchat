fce66d94da4acda4295eea176f11f0fe994b9c92
Anton Santiago <kristofercortez21@gmail.com>
2016-09-06 11:09:21 +0800

    created front end validations for register and login

--- app/routes/auth/register.js
+++ app/routes/auth/register.js
@@ -1,4 +1,12 @@
 import Ember from 'ember';
 
 export default Ember.Route.extend({
+  actions: {
+    doRegister() {
+      alert('registration attempted');
+    }
+  },
+  model() {
+    return this.store.createRecord('user');
+  }
 });

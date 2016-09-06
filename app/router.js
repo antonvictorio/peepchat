ff410c3afa4798b2efdf62f55f33b20f7d17560c
Anton Santiago <kristofercortez21@gmail.com>
2016-09-06 14:10:48 +0800

    users can now login after register

--- app/router.js
+++ app/router.js
@@ -2,15 +2,16 @@ import Ember from 'ember';
 import config from './config/environment';
 
 const Router = Ember.Router.extend({
-  location: config.locationType,
-  rootURL: config.rootURL
+    location: config.locationType,
+    rootURL: config.rootURL
 });
 
 Router.map(function() {
   this.route('auth', function() {
-    this.route('login');
-    this.route('register');
+      this.route('login');
+      this.route('register');
   });
+  this.route('app', function() {});
 });
 
 export default Router;

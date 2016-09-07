90799fa7b278a4ddc2f85f9e1128f37e95d761fe
Anton Santiago <kristofercortez21@gmail.com>
2016-09-07 15:04:50 +0800

    updated env

--- config/environment.js
+++ config/environment.js
@@ -53,7 +53,7 @@ module.exports = function(environment) {
   }
 
   if (environment === 'production') {
-    ENV.DS.host = 'https://quiet-river-84850.herokuapp.com/';
+    ENV.DS.host = 'https://radiant-crag-90533.herokuapp.com';
   }
 
   return ENV;
